d3d09688cb088309d35cd15a717111ad3e747ddb
Rakunho <rakatongo@hotmail.com>
2012-12-05 01:33:58 -0300

    Mast

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jHtmlArea.ColorPickerMenu-0.7.0.min
 //= require jHtmlArea-0.7.5.min
+//= require jHtmlArea.ColorPickerMenu-0.7.0.min
 //= require_tree .
