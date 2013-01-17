e69b68389a87513b6460508f4c184f2bf220e29e
rakatongo <rakatongo@hotmail.com>
2013-01-17 12:46:55 -0200

    Update config/initializers/nau.rb

--- config/initializers/nau.rb
+++ config/initializers/nau.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   #:domain               => "railscasts.com",
   :user_name            => "rakatongo@gmail.com",
-  :password             => "4246032",
+  :password             => "*******",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
